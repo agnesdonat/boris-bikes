ea7312dcf086ae9fd26cfc6c650c61e98f13c3f3
Agnes Donat <agnesdonat@Agness-MacBook-Pro.local>
2018-02-20 15:20:39 +0000

    Respond to release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,13 @@
 require 'docking_station'
 
+docking_station = DockingStation.new
+
 describe DockingStation do
   it 'checks if docking station exists' do
-    docking_station = DockingStation.new
     expect(docking_station).to be_an_instance_of(DockingStation)
+  end
+
+  it 'checks if docking_station releases a bike' do
+    expect(docking_station).to respond_to(:release_bike)
   end
 end
